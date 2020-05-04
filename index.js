b8c81e0d6d15d7933b8eb7d368cbfeb6439bdb6a
Paul McCarthy <paul@electricityexchange.ie>
2020-05-04 22:44:00 +0100

    Refactor to make it easier to test. Add basic tests

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const app = require('./src/app');
+const config = require('config');
+const Store = require('./src/Store');
+const logger = require('./src/Logger');
+const App = require('./src/App');
+
+const storeConfig = config.get('store');
+const appConfig = config.get('app');
+
+const store = Store(storeConfig, logger);
+const app = App(store, appConfig, logger);
 
 app.start();
