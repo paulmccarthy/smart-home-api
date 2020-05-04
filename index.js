7ad66ec25afdf256587e9de12de9a8d24eb6ecbe
Paul McCarthy <paul@electricityexchange.ie>
2020-05-04 22:45:18 +0100

    Fix entrypoint configs

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const logger = require('./src/Logger');
 const App = require('./src/App');
 
 const storeConfig = config.get('store');
-const appConfig = config.get('app');
 
 const store = Store(storeConfig, logger);
-const app = App(store, appConfig, logger);
+const app = App(store, config, logger);
 
 app.start();
